c3b889f883e4dfacd82054970589eeb29ebffd64
Deborah-R <deborah_rimei@hotmail.com>
2022-11-09 09:04:45 +0000

    Intent button main page with second activity

--- app/src/main/java/com/example/deborahrimei_3015579_trackingapp/MainActivity.kt
+++ app/src/main/java/com/example/deborahrimei_3015579_trackingapp/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.deborahrimei_3015579_trackingapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var _button : Button = findViewById(R.id.create_button)
+        _button.setOnClickListener {
+            val intent : Intent = Intent(this@MainActivity, CreateHabit::class.java)
+            startActivity(intent)
+        }
     }
 }
