9b854fe6be313d1ac6daaaca20bab1acfa7fd586
Deborah-R <deborah_rimei@hotmail.com>
2022-11-24 13:52:31 +0000

    Added function that saves items in the database by clicking button Save

--- app/src/main/java/com/example/deborahrimei_3015579_trackingapp/CreateActivity.kt
+++ app/src/main/java/com/example/deborahrimei_3015579_trackingapp/CreateActivity.kt
@@ -3,34 +3,60 @@ package com.example.deborahrimei_3015579_trackingapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.deborahrimei_3015579_trackingapp.database.DatabaseOperations
 
 class CreateActivity : AppCompatActivity() {
 
-    private var _buttonSaveHabit: Button? = null
-    private var _name: EditText? = null
-    private var _goal: EditText? = null
-    private var _person: EditText? = null
+    private lateinit var buttonSaveHabit: Button
+    private lateinit var habitNameEditText: EditText
+    private lateinit var habitReasonEditText: EditText
+    private lateinit var isCompleted: CheckBox
+    private lateinit var date: String
 
+
+    // create an habit
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create)
+        //change name of support bar
+        supportActionBar?.setTitle("Create Habit")
+
+        habitNameEditText = findViewById(R.id.create_et_name)
+        habitReasonEditText = findViewById(R.id.create_et_reason)
+
 
         //when click on save button,data will be saved and display on main activity
-        _buttonSaveHabit = findViewById(R.id.crete_btn_save)
-        _buttonSaveHabit?.setOnClickListener {
-            _name = findViewById(R.id.create_et_name)
-            _goal = findViewById(R.id.create_et_goal)
-            _person = findViewById(R.id.create_et_person)
+//        buttonSaveHabit = findViewById(R.id.crete_btn_save)
+//        buttonSaveHabit.setOnClickListener {
+//            Intent(this@CreateActivity, MainActivity::class.java).also {
+//                it.putExtra("EXTRA_NAME", habitNameEditText.text.toString())
+//                it.putExtra("EXTRA_GOAL", habitReasonEditText.text.toString())
+//                startActivity(it)
+//            }
+//        }
+    }
 
-            Intent(this@CreateActivity, MainActivity::class.java).also {
-                it.putExtra("EXTRA_NAME", _name?.text.toString())
-                it.putExtra("EXTRA_GOAL", _goal?.text.toString())
-                it.putExtra("EXTRA_PERSON", _person?.text.toString())
-                startActivity(it)
-            }
+    /* === SAVE HABIT === */
+    public fun saveHabitAction(view: View) {
+        val habitName = habitNameEditText.text.toString()
+        val habitReason = habitReasonEditText.text.toString()
+        val newHabit = Habit(habitName, habitReason, false)
+
+        val dbo = DatabaseOperations(this)
+        dbo.addItem(dbo, newHabit)
+
+        //return to the home page
+        Intent(this, MainActivity::class.java).also {
+            startActivity(it)
+            finish()
         }
+
+
     }
 
 }
