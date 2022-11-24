36e286e3b73564c28ce9930638b837a40d3c07e7
Deborah-R <deborah_rimei@hotmail.com>
2022-11-24 13:24:37 +0000

    Created queries for CREATE and DELETE a SQL Table

--- app/src/main/java/com/example/deborahrimei_3015579_trackingapp/database/DatabaseQueries.kt
+++ app/src/main/java/com/example/deborahrimei_3015579_trackingapp/database/DatabaseQueries.kt
@@ -5,13 +5,23 @@ import android.provider.BaseColumns
 object DatabaseQueries {
 
     // query that will initiliase the table
-    const val SQL_CREATE_TABLE_QUERY =""
+    const val SQL_CREATE_TABLE_QUERY =
+        "CREATE TABLE ${TableInfo.TABLE_NAME} (" +
+                "${BaseColumns._ID} INTEGER PRIMARY KEY," +
+                "${TableInfo.COLUMN_HABIT_NAME} TEXT," +
+                "${TableInfo.COLUMN_HABIT_REASON} TEXT," +
+                "${TableInfo.COLUMN_HABIT_COMPLETION} INTEGER," +
+                "${TableInfo.COLUMN_DATE} TEXT" + ")"
 
     // query that will delete the table
-    const val SQL_DELETE_TABLE_QUERY = ""
+    const val SQL_DELETE_TABLE_QUERY = "DROP TABLE IF EXISTS ${TableInfo.TABLE_NAME}"
 
     //create interface for the Table with all the habit fields
     object TableInfo : BaseColumns {
-
+        const val TABLE_NAME = "habitsTable"
+        const val COLUMN_HABIT_NAME = "habitName"
+        const val COLUMN_HABIT_REASON = "habitReason"
+        const val COLUMN_HABIT_COMPLETION = "habitCompletion"
+        const val COLUMN_DATE = "habitDate"
     }
 }
