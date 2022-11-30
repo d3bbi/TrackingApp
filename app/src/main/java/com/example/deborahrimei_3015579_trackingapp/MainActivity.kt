65f87e137c86dee8c50c85b95a3957e7cb1fedc8
Deborah-R <deborah_rimei@hotmail.com>
2022-11-30 21:36:15 +0000

    added button with intent to the Profile page

--- app/src/main/java/com/example/deborahrimei_3015579_trackingapp/MainActivity.kt
+++ app/src/main/java/com/example/deborahrimei_3015579_trackingapp/MainActivity.kt
@@ -5,23 +5,26 @@ import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.widget.Button
-import android.widget.CheckBox
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.deborahrimei_3015579_trackingapp.database.DatabaseOperations
 import com.example.deborahrimei_3015579_trackingapp.database.DatabaseQueries
+import com.example.deborahrimei_3015579_trackingapp.habits.CreateActivity
+import com.example.deborahrimei_3015579_trackingapp.habits.Habit
+import com.example.deborahrimei_3015579_trackingapp.habits.HabitMainAdapter
+import com.example.deborahrimei_3015579_trackingapp.habits.StatusActivity
+import com.example.deborahrimei_3015579_trackingapp.user.UserProfile
 
 class MainActivity : AppCompatActivity() {
 
     // variable used
     private lateinit var buttonCreatehabit: Button
     private lateinit var buttonSeeCalendar: Button
+    private lateinit var buttonProfile: Button
     private lateinit var initialIdentity: TextView
     private lateinit var initialName: TextView
-    private lateinit var completedCheckbox : CheckBox
 
     // variable recycler view
     private lateinit var habitRecyclerView: RecyclerView
@@ -35,17 +38,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
 
+
         /* === ADD HABITS IN THE DATABASE === */
         val dbo = DatabaseOperations(this)
         val cursor = dbo.getAllHabits(dbo)
 
-        with(cursor){
-            while (moveToNext()){
-                val habitName = getString(getColumnIndexOrThrow(DatabaseQueries.TableInfo.COLUMN_HABIT_NAME))
-                val habitReason = getString(getColumnIndexOrThrow(DatabaseQueries.TableInfo.COLUMN_HABIT_REASON))
-                val habitCompletion = getString(getColumnIndexOrThrow(DatabaseQueries.TableInfo.COLUMN_HABIT_COMPLETION))
-                val habitIsCompleted = if (habitCompletion.toInt() == 0 ) false else true
-                val habitDate = getString(getColumnIndexOrThrow(DatabaseQueries.TableInfo.COLUMN_DATE))
+        with(cursor) {
+            while (moveToNext()) {
+                val habitName =
+                    getString(getColumnIndexOrThrow(DatabaseQueries.HabitTable.COLUMN_HABIT_NAME))
+                val habitReason =
+                    getString(getColumnIndexOrThrow(DatabaseQueries.HabitTable.COLUMN_HABIT_REASON))
+                val habitCompletion =
+                    getString(getColumnIndexOrThrow(DatabaseQueries.HabitTable.COLUMN_HABIT_COMPLETION))
+                val habitIsCompleted = if (habitCompletion.toInt() == 0) false else true
+                val habitDate =
+                    getString(getColumnIndexOrThrow(DatabaseQueries.HabitTable.COLUMN_DATE))
 
                 habitList.add(Habit(habitName, habitReason, habitIsCompleted))
                 dbo.close()
@@ -79,7 +87,6 @@ class MainActivity : AppCompatActivity() {
         buttonSeeCalendar.setOnClickListener {
             val intent = Intent(this@MainActivity, StatusActivity::class.java)
             startActivity(intent)
-            finish()
         }
 
         /* === SHARE INITIAL INFORMATION WITH THE MAIN ACTIVITY ===
@@ -92,6 +99,13 @@ class MainActivity : AppCompatActivity() {
         var _name = preferences.getString("Username", "").toString()
         initialIdentity.setText(_identity)
         initialName.setText("Hi $_name")
-    }
 
+
+        buttonProfile = findViewById(R.id.main_btn_profile)
+        buttonProfile.setOnClickListener {
+            val intent = Intent(this@MainActivity, UserProfile::class.java)
+            startActivity(intent)
+        }
+
+    }
 }
